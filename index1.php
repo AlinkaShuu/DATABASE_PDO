265c67c93b1bce2159b3fdc68bd13a34c27d8bb3
sinutkamixey <sinutkamixey>
2017-12-06 19:51:14 +0700

    Выводим всех студентов с базы данных, котрая находится в PHPMyAdmin

---
+++ index1.php
@@ -0,0 +1,17 @@
+<?php	
+$dbParams=require(
+	'db.php'
+);
+	
+$db=new PDO (
+	"mysql:host=localhost;dbname=".$dbParams['database'].";charset=utf8",
+	$dbParams['username'],
+	$dbParams['password']
+);
+$students=$db
+    ->query('SELECT * FROM `student`')
+    ->fetchAll(PDO::FETCH_ASSOC); //возвращается ассоциативный массив, ключи — имена полей
+Foreach ($students as $student){
+	echo "<p>".htmlspecialchars($student['lastName'])."</p>";
+}
+?>
